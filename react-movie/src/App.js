c5b1d8c3cf324660e98ff6da1f98e14238587b88
timiaraines <72035201+timiaraines@users.noreply.github.com>
2022-09-16 18:51:29 -0500

    adds searhbox and header functionality

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,33 +1,39 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 function App() {
-  const [movies, setMovies] = useState([      {
-    "Title": "Star Wars",
-    "Year": "1977",
-    "imdbID": "tt0076759",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode V - The Empire Strikes Back",
-    "Year": "1980",
-    "imdbID": "tt0080684",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode VI - Return of the Jedi",
-    "Year": "1983",
-    "imdbID": "tt0086190",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-},]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+const getMovieRequest = async() =>{
+  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a615223a`;
+
+  const response = await fetch(url);
+  const responseJson = await response.json();
+
+  if(responseJson.Search) {
+    setMovies(responseJson.Search);
+  };
+};
+
+useEffect(()=>{
+  getMovieRequest(searchValue);
+}, [searchValue]);
   return (
-    <div>
-      <MovieList movies={movies}/>
+    <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+        <div className='row'>
+        <MovieList movies={movies}/>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
