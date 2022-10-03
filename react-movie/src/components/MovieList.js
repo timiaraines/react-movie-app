158f979fbac41570c42b53d2027379f014c8aaed
timiaraines <72035201+timiaraines@users.noreply.github.com>
2022-10-03 18:15:00 -0500

    adds add favorites click

--- react-movie/src/components/MovieList.js
+++ react-movie/src/components/MovieList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const MovieList = (props) => {
+    const favoriteComponent = props.favoriteComponent;
     return(
         <>
-        {props.movies.map((movie,index)=> 
-        <div className='d-flex justify-content-start m-3'>
-            <img src={movie.Poster} alt= 'movie'></img>
-        </div>)}
+            {props.movies.map((movie,index)=> (
+                <div className='image-container d-flex justify-content-start m-3'>
+                    <img src={movie.Poster} alt= 'movie'></img>
+                    <div onClick={() => props.handleFavoritesClick()} className='overlay d-flex align-items-center justify-content-center'>
+                        <favoriteComponent/>
+                    </div>
+                </div>
+            ))}
         </>
     );
 };
